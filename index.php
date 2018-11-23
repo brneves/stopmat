2282b670c282314e43fa050920965fb327513bcc
Ronaldo Neves <ronaldo@ronaldoneves.com.br>
2018-11-22 21:25:11 -0300

    Finalizando para apenas uma resposta

--- index.php
+++ index.php
@@ -26,9 +26,9 @@
             <div class="collapse navbar-collapse text-right" id="menu">
                 <ul class="nav navbar-nav">
                     <li><a href="#sobre-nos">Sobre Nós</a></li>
-                    <li><a href="#nossos-projetos">Aprendendo</a></li>
-                    <li><a href="#depoimentos">Vamos jogar</a></li>
-                    <li><a href="#contato">Contato</a></li>
+                    <li><a href="#aprendendo">Aprendendo</a></li>
+                    <li><a href="#jogo">Vamos jogar</a></li>
+<!--                    <li><a href="#contato">Contato</a></li>-->
                 </ul>
             </div>
         </div>
@@ -99,7 +99,7 @@
     </div>
 </div>
 
-<section id="nossos-projetos" class="container">
+<section id="aprendendo" class="container">
     <h2 class="text-center">Aprendendo</h2>
     <div class="row">
         <div class="col-sm-6 col-md-4 col-lg-4">
@@ -178,7 +178,7 @@
 			$data = filter_input_array( INPUT_POST, FILTER_DEFAULT );
 			if ( isset( $data ) ):
 
-				var_dump( $data );
+//				var_dump( $data );
 //                $i = 0;
 				//FAZ OS CÁLCULOS
 				//faz o loop com os dados enviados
@@ -188,153 +188,214 @@
 
 //					for ( $i = 0; $i <= 5; $i ++ ):
 
-						//verifica a resposta da soma
-						$respostaSoma       = $numero + $data['soma'];
-						$respostaSub        = $numero - $data['subtracao'];
-						$respostaSucessor   = $numero + 1;
-						$respostaAntecessor = $numero - 1;
-						$respostaMult       = $numero * $data['multiplicacao'];
-						$respostaDivisao    = $numero / $data['divisao'];
+					//verifica a resposta da soma
+					$respostaSoma       = $numero + $data['soma'];
+					$respostaSub        = $numero - $data['subtracao'];
+					$respostaSucessor   = $numero + 1;
+					$respostaAntecessor = $numero - 1;
+					$respostaMult       = $numero * $data['multiplicacao'];
+					$respostaDivisao    = $numero / $data['divisao'];
 
-						echo $data[ 'resp'][ 0 ] . " - " . $respostaSoma . "<br>";
-						echo $data[ 'resp'][ 1 ] . " - " . $respostaSub . "<br>";
-						echo $data[ 'resp'][ 2 ] . " - " . $respostaSucessor . "<br>";
-						echo $data[ 'resp'][ 3 ] . " - " . $respostaMult . "<br>";
-						echo $data[ 'resp'][ 4 ] . " - " . $respostaAntecessor . "<br>";
-						echo $data[ 'resp'][ 5 ] . " - " . $respostaDivisao . "<br>";
+					$respSoma = $data['resp'][0];
+					$respSub  = $data['resp'][1];
+					$respSuc  = $data['resp'][2];
+					$respMult = $data['resp'][3];
+					$respAnt  = $data['resp'][4];
+					$respDiv  = $data['resp'][5];
 
-						if ( $data[ 'resp'][0] == $respostaSoma ) {
-							$pontuacao += 10;
-							echo "adicao<br>";
-						}
-						if ( $data[ 'resp'][1] == $respostaSub ) {
-							$pontuacao += 10;
-							echo "subtracao<br>";
-						}
-						if ( $data[ 'resp'][2] == $respostaSucessor ) {
-							$pontuacao += 10;
-							echo "antecessor<br>";
-						}
-						if ( $data[ 'resp'][3] == $respostaMult ) {
-							$pontuacao += 10;
-							echo "sucessor<br>";
-						}
-						if ( $data[ 'resp'][4] == $respostaAntecessor ) {
-							$pontuacao += 10;
-							echo "multiplicacao<br>";
-						}
-						if ( $data[ 'resp'][5] == $respostaDivisao ) {
-							$pontuacao += 10;
-							echo "divisao<br>";
-						}
+//					echo $data['resp'][0] . " - " . $respostaSoma . "<br>";
+//					echo $data['resp'][1] . " - " . $respostaSub . "<br>";
+//					echo $data['resp'][2] . " - " . $respostaSucessor . "<br>";
+//					echo $data['resp'][3] . " - " . $respostaMult . "<br>";
+//					echo $data['resp'][4] . " - " . $respostaAntecessor . "<br>";
+//					echo $data['resp'][5] . " - " . $respostaDivisao . "<br>";
 
-						/*
-						echo $data[ 'resp' . $contNum ][ $i ] . "<br>";
+					if ( $data['resp'][0] == $respostaSoma ) {
+						$pontuacao += 10;
+						$classAdicao = 'success';
+					}
+					if ( $data['resp'][1] == $respostaSub ) {
+						$pontuacao += 10;
+						$classSub = 'success';
+					}
+					if ( $data['resp'][2] == $respostaSucessor ) {
+						$pontuacao += 10;
+						$classSuc = 'success';
+					}
+					if ( $data['resp'][3] == $respostaMult ) {
+						$pontuacao += 10;
+						$classMult = 'success';
+					}
+					if ( $data['resp'][4] == $respostaAntecessor ) {
+						$pontuacao += 10;
+						$classAnt = 'success';
+					}
+					if ( $data['resp'][5] == $respostaDivisao ) {
+						$pontuacao += 10;
+						$classDiv = 'success';
+					}
 
-						if ( $data[ 'resp' . $contNum ][0] == $respostaSoma ) {
-							$pontuacao += 10;
-						}
-						if ( $data[ 'resp' . $contNum ][1] == $respostaSub ) {
-							$pontuacao += 10;
-						}
-						if ( $data[ 'resp' . $contNum ][2] == $respostaAntecessor ) {
-							$pontuacao += 10;
-						}
-						if ( $data[ 'resp' . $contNum ][3] == $respostaSucessor ) {
-							$pontuacao += 10;
-						}
-						if ( $data[ 'resp' . $contNum ][4] == $respostaMult ) {
-							$pontuacao += 10;
-						}
-						if ( $data[ 'resp' . $contNum ][5] == $respostaDivisao ) {
-							$pontuacao += 10;
-						}
-						*/
+					/*
+					echo $data[ 'resp' . $contNum ][ $i ] . "<br>";
+
+					if ( $data[ 'resp' . $contNum ][0] == $respostaSoma ) {
+						$pontuacao += 10;
+					}
+					if ( $data[ 'resp' . $contNum ][1] == $respostaSub ) {
+						$pontuacao += 10;
+					}
+					if ( $data[ 'resp' . $contNum ][2] == $respostaAntecessor ) {
+						$pontuacao += 10;
+					}
+					if ( $data[ 'resp' . $contNum ][3] == $respostaSucessor ) {
+						$pontuacao += 10;
+					}
+					if ( $data[ 'resp' . $contNum ][4] == $respostaMult ) {
+						$pontuacao += 10;
+					}
+					if ( $data[ 'resp' . $contNum ][5] == $respostaDivisao ) {
+						$pontuacao += 10;
+					}
+					*/
 //					endfor;
 
 					$contNum ++;
 
 				endforeach;
 
-				echo "<h1>{$pontuacao}</h1>";
+				?>
 
-			endif;
-
-			//define os números
-			$numeros = [
-				rand( 1, 20 ),
-				rand( 1, 20 ),
-				rand( 1, 20 ),
-				rand( 1, 20 )
-			];
-			//sorteia o número para o jogo
-			$numeroJogo = rand( 2, 12 );
-
-			//define os números das operações
-			$soma          = rand( 1, 50 );
-			$subtracao     = rand( 1, $numeroJogo );
-			$multiplicacao = rand( 1, 5 );
-
-			//verifica se o número sorteado é par ou ímpar para determinar a divisão
-			if ( $numeroJogo % 2 == 0 ):
-				if ( $numeroJogo > 2 ):
-                    $num = ['2', '4'];
-				    $rand = array_rand($num, 1);
-					$divisao = $num[$rand];
-                else:
-	                $divisao = 2;
-				endif;
-			else:
-				$num = ['1', '3'];
-				$rand = array_rand($num, 1);
-				$divisao = $num[$rand];
-			endif;
-
-			?>
-            <form action="#jogo" method="post">
-                <!-- ENVIA OS DADOS DAS SOMAS -->
-                <input type="hidden" name="soma" value="<?= $soma ?>">
-                <input type="hidden" name="subtracao" value="<?= $subtracao ?>">
-                <input type="hidden" name="multiplicacao" value="<?= $multiplicacao ?>">
-                <input type="hidden" name="divisao" value="<?= $divisao ?>">
-
-                <table class="table table-striped table-bordered">
+                <table class="table">
                     <thead>
                     <tr>
-                        <th width="50px"></th>
-                        <th class="text-center backSoma">+<?= $soma; ?></th>
-                        <th class="text-center backSubtracao">-<?= $subtracao ?></th>
-                        <th class="text-center backSucessor">Sucessor <i class="fas fa-share"></i></th>
-                        <th class="text-center backMultiplicacao">x<?= $multiplicacao ?></th>
-                        <th class="text-center backAntecessor"><i class="fas fa-reply"></i> Antecessor</th>
-                        <th class="text-center backDivisao">&divide;<?= $divisao ?></th>
+                        <th colspan="3" class="text-center"><h1>Sua pontuação foi <strong><?= $pontuacao; ?></strong></h1></th>
+                    </tr>
+                    <tr>
+                        <th>Operação</th>
+                        <th>Sua Resposta</th>
+                        <th>Resposta Correta</th>
                     </tr>
                     </thead>
                     <tbody>
-					<?php
-
-					//					for ( $i = 0; $i < 3; $i ++ ):
-
-
-					?>
-                    <input type="hidden" name="num[]" value="<?= $numeroJogo; ?>">
-                    <tr>
-                        <td><?= $numeroJogo; ?></td>
-                        <td><input name="resp[]" type="text" class="form-control"></td>
-                        <td><input name="resp[]" type="text" class="form-control"></td>
-                        <td><input name="resp[]" type="text" class="form-control"></td>
-                        <td><input name="resp[]" type="text" class="form-control"></td>
-                        <td><input name="resp[]" type="text" class="form-control"></td>
-                        <td><input name="resp[]" type="text" class="form-control"></td>
+                    <tr class="<?= isset($classAdicao) ? $classAdicao : 'danger'; ?>">
+                        <td>Adição</td>
+                        <td><?= $respSoma ?></td>
+                        <td><?= $respostaSoma ?></td>
                     </tr>
-                    <!--					--><?php //endfor; ?>
+                    <tr class="<?= isset($classSub) ? $classSub : 'danger'; ?>">
+                        <td>Subtração</td>
+                        <td><?= $respSub ?></td>
+                        <td><?= $respostaSub ?></td>
+                    </tr>
+                    <tr class="<?= isset($classSuc) ? $classSuc : 'danger'; ?>">
+                        <td>Sucessor</td>
+                        <td><?= $respSuc ?></td>
+                        <td><?= $respostaSucessor ?></td>
+                    </tr>
+                    <tr class="<?= isset($classMult) ? $classMult : 'danger'; ?>">
+                        <td>Multiplicação</td>
+                        <td><?= $respMult ?></td>
+                        <td><?= $respostaMult ?></td>
+                    </tr>
+                    <tr class="<?= isset($classAnt) ? $classAnt : 'danger'; ?>">
+                        <td>Antecessor</td>
+                        <td><?= $respAnt ?></td>
+                        <td><?= $respostaAntecessor ?></td>
+                    </tr>
+                    <tr class="<?= isset($classDiv) ? $classDiv : 'danger'; ?>">
+                        <td>Divisão</td>
+                        <td><?= $respDiv ?></td>
+                        <td><?= $respostaDivisao ?></td>
+                    </tr>
                     </tbody>
                 </table>
 
-                <div class="col-md-12 text-center">
-                    <button class="btn btn-success btn-lg">Stop</button>
-                </div>
-            </form>
+                <a href="http://localhost/stopmat" class="btn btn-lg btn-success text-center">Jogar novamente</a>
+            
+				<?php
+
+			else: 
+
+				//define os números
+				$numeros = [
+					rand( 1, 20 ),
+					rand( 1, 20 ),
+					rand( 1, 20 ),
+					rand( 1, 20 )
+				];
+				//sorteia o número para o jogo
+				$numeroJogo = rand( 2, 12 );
+
+				//define os números das operações
+				$soma          = rand( 1, 50 );
+				$subtracao     = rand( 1, $numeroJogo );
+				$multiplicacao = rand( 1, 5 );
+
+				//verifica se o número sorteado é par ou ímpar para determinar a divisão
+				if ( $numeroJogo % 2 == 0 ):
+					if ( $numeroJogo > 2 ):
+						$num     = [ '2', '4' ];
+						$rand    = array_rand( $num, 1 );
+						$divisao = $num[ $rand ];
+					else:
+						$divisao = 2;
+					endif;
+				else:
+					$num     = [ '1', '3' ];
+					$rand    = array_rand( $num, 1 );
+					$divisao = $num[ $rand ];
+				endif;
+
+
+				?>
+                <form action="#jogo" method="post">
+                    <!-- ENVIA OS DADOS DAS SOMAS -->
+                    <input type="hidden" name="soma" value="<?= $soma ?>">
+                    <input type="hidden" name="subtracao" value="<?= $subtracao ?>">
+                    <input type="hidden" name="multiplicacao" value="<?= $multiplicacao ?>">
+                    <input type="hidden" name="divisao" value="<?= $divisao ?>">
+
+                    <table class="table table-striped table-bordered">
+                        <thead>
+                        <tr>
+                            <th width="50px"></th>
+                            <th class="text-center backSoma">+<?= $soma; ?></th>
+                            <th class="text-center backSubtracao">-<?= $subtracao ?></th>
+                            <th class="text-center backSucessor">Sucessor <i class="fas fa-share"></i></th>
+                            <th class="text-center backMultiplicacao">x<?= $multiplicacao ?></th>
+                            <th class="text-center backAntecessor"><i class="fas fa-reply"></i> Antecessor</th>
+                            <th class="text-center backDivisao">&divide;<?= $divisao ?></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+						<?php
+
+						//					for ( $i = 0; $i < 3; $i ++ ):
+
+
+						?>
+                        <input type="hidden" name="num[]" value="<?= $numeroJogo; ?>">
+                        <tr>
+                            <td><?= $numeroJogo; ?></td>
+                            <td><input name="resp[]" type="text" class="form-control"></td>
+                            <td><input name="resp[]" type="text" class="form-control"></td>
+                            <td><input name="resp[]" type="text" class="form-control"></td>
+                            <td><input name="resp[]" type="text" class="form-control"></td>
+                            <td><input name="resp[]" type="text" class="form-control"></td>
+                            <td><input name="resp[]" type="text" class="form-control"></td>
+                        </tr>
+                        <!--					--><?php //endfor;
+						?>
+                        </tbody>
+                    </table>
+
+                    <div class="col-md-12 text-center">
+                        <button class="btn btn-success btn-lg">Stop</button>
+                    </div>
+                </form>
+
+			<?php endif; ?>
+
         </div>
     </div>
 </section>
